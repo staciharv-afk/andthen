2607bdb9147203891e7e551bd5344d4b37c8dd65
staciharv-afk <staciharv@gmail.com>
2026-03-11 21:29:41 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AndThenApp from './App.jsx'
+import AndThenApp from './app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
